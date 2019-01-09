c13c9f76513cb27c1650e8430a5d2f52501ad407
James Strachan <james.strachan@gmail.com>
2019-01-09 15:44:47 +0000

    chore: typo

--- jx/scripts/release.sh
+++ jx/scripts/release.sh
@@ -6,7 +6,7 @@ jx step bdd -b  --provider=gke --git-provider=ghe --git-provider-url=https://git
 
 echo "How storing the test results"
 
-# TODO tranform junit.xml into a HTML report via https://github.com/jenkins-x-images/xunit-viewer
+# TODO transform junit.xml into a HTML report via https://github.com/jenkins-x-images/xunit-viewer
 
 jx step collect -c tests -p "reports/junit.xml"
 
