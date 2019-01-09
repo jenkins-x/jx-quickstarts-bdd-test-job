628547c06f82b40c98586d7fe7fb6f3ccf3b602b
James Strachan <james.strachan@gmail.com>
2019-01-09 16:44:51 +0000

    more logging

--- jx/scripts/release.sh
+++ jx/scripts/release.sh
@@ -11,9 +11,11 @@ echo "starting the BDD Quickstart tests"
 
 jx step bdd -b  --provider=gke --git-provider=ghe --git-provider-url=https://github.beescloud.com --git-username dev1 --git-api-token $GHE_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring --ignore-fail
 
-echo "How storing the test results on stable storage"
+echo "How storing the test results on stable storage..."
 
 # TODO transform junit.xml into a HTML report via https://github.com/jenkins-x-images/xunit-viewer
 
 jx step collect -c tests -p "reports/junit.xml"
+
+echo "Done!"
 
