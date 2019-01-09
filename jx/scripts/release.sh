d1e1e86743c3ba086d3de6a5d740aec1edeba9bf
James Strachan <james.strachan@gmail.com>
2019-01-09 16:44:13 +0000

    more logging

--- jx/scripts/release.sh
+++ jx/scripts/release.sh
@@ -7,7 +7,7 @@ jx version -b
 export $GHE_CREDS_PSW="$(jx step credential -s jx-pipeline-git-github-ghe -k password)"
 export $JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user -k password)"
 
-echo "starting the BDD tests"
+echo "starting the BDD Quickstart tests"
 
 jx step bdd -b  --provider=gke --git-provider=ghe --git-provider-url=https://github.beescloud.com --git-username dev1 --git-api-token $GHE_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring --ignore-fail
 
