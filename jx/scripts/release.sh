b56c129e9857135a8fc4ba8d2de6b8f1eeb1228c
James Strachan <james.strachan@gmail.com>
2019-01-09 15:51:07 +0000

    chore: typo

--- jx/scripts/release.sh
+++ jx/scripts/release.sh
@@ -4,7 +4,7 @@ echo "Running BDD tests for Quickstarts"
 
 jx step bdd -b  --provider=gke --git-provider=ghe --git-provider-url=https://github.beescloud.com --git-username dev1 --git-api-token $GHE_CREDS_PSW --default-admin-password $JENKINS_CREDS_PSW --no-delete-app --no-delete-repo --tests install --tests test-create-spring --ignore-fail
 
-echo "How storing the test results"
+echo "How storing the test results on stable storage"
 
 
 # TODO transform junit.xml into a HTML report via https://github.com/jenkins-x-images/xunit-viewer
