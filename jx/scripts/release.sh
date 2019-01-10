6ac9fc6e7feaabecbc5eb77d4ad57a2fc0504f76
James Strachan <james.strachan@gmail.com>
2019-01-10 08:01:51 +0000

    setup kube context before running the bdd tests

--- jx/scripts/release.sh
+++ jx/scripts/release.sh
@@ -6,6 +6,16 @@ jx version -b
 
 export GHE_CREDS_PSW="$(jx step credential -s jx-pipeline-git-github-ghe -k password)"
 export JENKINS_CREDS_PSW="$(jx step credential -s  test-jenkins-user -k password)"
+export GKE_SA="$(jx step credential -s gke-sa -k password)"
+
+
+// setup kube context and git
+
+gcloud auth activate-service-account --key-file $GKE_SA
+gcloud container clusters get-credentials anthorse --zone europe-west1-b --project jenkinsx-dev
+
+git config --global --add user.name JenkinsXBot"
+git config --global --add user.email jenkins-x@googlegroups.com"
 
 echo "starting the BDD Quickstart tests"
 
@@ -15,7 +25,7 @@ echo "storing the test results on stable storage..."
 
 # TODO transform junit.xml into a HTML report via https://github.com/jenkins-x-images/xunit-viewer
 
-jx step collect -c tests -p "reports/junit.xml"
+jx step collect -c tests -p "reports/junit.xml" --git-url https://github.com/jenkins-x/jx-devops-results.git
 
 echo "Done!"
 
