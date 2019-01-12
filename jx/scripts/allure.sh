d72cb8b755f83b0aed6920872e2a53d935f1eed9
James Strachan <james.strachan@gmail.com>
2019-01-12 16:18:12 +0000

    first spike at allure report generation

---
+++ jx/scripts/allure.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "Generating the Allure Report from the test results"
+
+echo "Using allure: $(allure --version)"
+
+git config --global --add user.name JenkinsXBot
+git config --global --add user.email jenkins-x@googlegroups.com
+
+git clone -b gh-pages https://github.com/jenkins-x/jx-devops-results.git
+
+cd jx-devops-results/jenkins-x/tests/jenkins-x/jx-devops-results
+
+echo "Generating allure report"
+pwd
+
+allure generate --clean qs -o allure
+
+echo "Now adding files to git"
+
+git add allure
+git commit -m "updated Allure report"
+git push
+
+echo "Pushed changes"
+
+
+
+
+
+
